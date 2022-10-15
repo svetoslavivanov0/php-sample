1d3f9508a46a1ce9ebdae8d494f23243d3657dfa
sivanov <sivanov@2create.studio>
2022-10-15 10:41:51 +0300

    Add migrations package

---
+++ phoenix.php
@@ -0,0 +1,34 @@
+<?php
+
+// load .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+return [
+    'migration_dirs' => [
+        'first' => __DIR__ . '/app/database/migrations',
+    ],
+    'environments' => [
+        'local' => [
+            'adapter' => 'mysql',
+            'host' => $_ENV['DB_HOST'],
+            'db_name' => $_ENV['DB_NAME'],
+            'username' => $_ENV['DB_USER'],
+            'password' => $_ENV['DB_PASSWORD'],
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_general_ci', // optional, if not set default collation for utf8mb4 is used
+        ],
+        'production' => [
+            'adapter' => 'mysql',
+            'host' => 'production_host',
+            'port' => 3306, // optional
+            'username' => 'user',
+            'password' => 'pass',
+            'db_name' => 'my_production_db',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_general_ci', // optional, if not set default collation for utf8mb4 is used
+        ],
+    ],
+    'default_environment' => 'local',
+    'log_table_name' => 'phoenix_log',
+];
