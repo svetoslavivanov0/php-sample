e1914ed0353a8365e7ebc590622ab19f7a947f03
sivanov <sivanov@2create.studio>
2022-10-15 12:38:50 +0300

    Add db migrations

---
+++ database/migrations/20221015093539_create_users_table.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Phoenix\Migration\AbstractMigration;
+
+final class CreateUsersTable extends AbstractMigration
+{
+    protected function up(): void
+    {
+        $this->table('users')
+            ->addColumn('username', 'string')
+            ->addColumn('password', 'string')
+            ->addColumn('email', 'string')
+            ->addColumn('created_at', 'datetime')
+            ->create();
+    }
+
+    protected function down(): void
+    {
+        $this->table('users')
+            ->drop();
+    }
+}
